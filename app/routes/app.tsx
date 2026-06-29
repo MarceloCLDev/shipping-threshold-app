95d4da01054d3d273f1acd9a5e02a5afc96099ec
MarceloCLDev <marcelo.n@perfumeventures.com>
2026-06-29 18:08:41 -0300

    Updating admin nav menu

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -18,8 +18,8 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/banner-test">Banner Test</s-link>
+        <s-link href="/app">Shipping Threshold</s-link>
+        <s-link href="/app/banner-test">Checkout Disclaimer Banner</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
